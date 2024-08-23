af75e7e136e12851135f1b6dc2cec3a16723c55b
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-08-23 13:34:22 -0300

    adding composer and autoload

--- public/index.php
+++ public/index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <?php
-        echo("Batman");
-    ?>
-</head>
-<body>
+<?php
+    require '../vendor/autoload.php';
     
-</body>
-</html>
+?>
