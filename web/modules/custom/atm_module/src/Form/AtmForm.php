3b50a415ef2f620d23280af3d7ddfc5572ac4759
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-09-02 09:23:52 -0300

    adding the logic of the atm withdraw

--- web/modules/custom/atm_module/src/Form/AtmForm.php
+++ web/modules/custom/atm_module/src/Form/AtmForm.php
@@ -55,13 +55,75 @@ class AtmForm extends FormBase {
 
         if($amount == 0) {
             // teste de erro
-            $this->messenger()->addError($this->t('Please enter a value greater than zero'));
+            $this->messenger()->addError($this->t('Please enter a value greater than zero')); 
         }else{
             // numero validaddo
+
+            $banknoteList = $this->withdraw($amount);
+            
+            $formattedAmount = number_format($amount, 2, ',', '.');
+            $message_parts = "<h1>Valor do saque R$$formattedAmount</hh1>";
+
+            foreach ($banknoteList as $value => $count) {
+                if($value == 'total'){
+                  $message_parts .= "<h3>Foram retiradas $count cédulas no total.</h3>";
+                  break;
+                }
+                if ($count > 0) {
+                  $formattedBanknote = number_format($value, 2, ',', '.');
+                  $message_parts .= "<h5>$count nota(s) de R$ $formattedBanknote</h5>";
+                }
+
+            }
+
             $this->messenger()->addMessage($this->t('You entered: $@amount', ['@amount' => $amount]));
-            $form_state->set('custom_message', "olaaaa");
+            $form_state->set('custom_message', $message_parts); // AQUI 
             $form_state->setRebuild(TRUE);
         }
 
+    }
+
+    public function withdraw($amount){
+        $banknote100 = 0;
+        $banknote50 = 0;
+        $banknote10 = 0;
+        $banknote5 = 0;
+        $banknote2 = 0;
+        $banknote1 = 0;
+
+        if($amount >= 100){
+          $banknote100 = intdiv($amount, 100);
+          $amount %= 100;
+        }
+        if($amount >= 50){
+          $banknote50 = intdiv($amount, 50);
+          $amount %= 50;
+        }
+        if($amount >= 10){
+          $banknote10 = intdiv($amount, 10);
+          $amount %= 10;
+        }
+        if($amount >= 5){
+          $banknote5 = intdiv($amount, 5);
+          $amount %= 5;
+        }
+        if($amount >= 2){
+          $banknote2 = intdiv($amount, 2);
+          $amount %= 2;
+        }
+        
+        if ($amount >= 1) {
+            $banknote1 = $amount;
+        }
+
+        return [
+          '100' => $banknote100,
+          '50' => $banknote50,
+          '10' => $banknote10,
+          '5' => $banknote5,
+          '2' => $banknote2,
+          '1' => $banknote1,
+          'total' => $banknote100+$banknote50+$banknote10+$banknote5+$banknote2+$banknote1
+        ];
     }
 }
